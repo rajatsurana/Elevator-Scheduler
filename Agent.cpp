9cd357b74f9d60307d2d970d4651127bedd893d5
rajatsurana <rajat.surana123@gmail.com>
2016-10-30 11:57:00 +0530

    State representation and input params

--- Agent.cpp
+++ Agent.cpp
@@ -14,9 +14,26 @@ class Argument{
 };
 Argument::Argument(){
 }
+class State{
+    public:
+    	int N;                        //number of floors
+    	int K;                        //number of elevators
+    	vector<int> pos;							// = [0]*K  initial positions of all elevators
+    	vector<int> BU;								// = [0]*N  # button up on each floor   (always 0 for top floor)
+    	vector<int> BD;              	// button down on each floor (always 0 for first floor)
+    	vector<vector<int> >BF;				//[K][N]  = [[0]*N for i in range(K)] -- floor buttons pressed inside elevator, for each elevator
+    	vector<int> dirn;							//1 or 0 for up or down---dirn for all elevators
+    	State(int N, int K):pos(K),BU(N), BD(N),BF(K),dirn(K){
+    	    for(int l=0;l<K;l++)
+                for(int i=0;i<N;i++){
+                    BF[l].push_back(0);
+                }
+    	};
+};
 void simpleAgent(Argument args){
+		State state(args.N,args.K);
     //ready = sys.stdin.readline().strip()
- 	char ready[100];
+ 		char ready[100];
     cin.getline(ready,100);
     //int data[3];
     //int i = 0;
@@ -39,19 +56,15 @@ void simpleAgent(Argument args){
 		repeat.push_back("AOD");
 	}
 		repeat.push_back("AOU");
-    //repeat = ['AU','AOU']*(args.N-1)
-    //repeat[-1] = 'AOD'
-   // repeat += ['AD','AOD']*(args.N-1)
-   // repeat[-1] = 'AOU'
 
     int i = 0;
 
     while(1){
 		vector<string> actions;
 		string suff="";
-				
+
 				stringstream sss;
-				
+
 		for(int k=0;k<args.K;k++){
 			int kk=k+1;
 			sss<< kk;
@@ -68,7 +81,7 @@ void simpleAgent(Argument args){
 				stringstream ss;
 				ss<< ll;
 				ss>>end;
-                actions[l] = repeat[(i - (args.N/args.K+1)*l*2 - 1) % repeat.size()] +end;//+ string(1,1);
+        actions[l] = repeat[(i - (args.N/args.K+1)*l*2 - 1) % repeat.size()] +end;//+ string(1,1);
                 //cerr<<(char)(l+1)<<" l+1 "<<l+1<<endl;
 			}
 		}
@@ -76,27 +89,28 @@ void simpleAgent(Argument args){
 		for(int ll=1;ll<actions.size();ll++){
 			actionsOut+= " "+ actions[ll];
 		}
-
         i+=1;
- 
 		cout<<actionsOut<<'\n'<<std::flush;
-
 		char updates[100];
     	cin.getline(updates,100);
+			cerr<<updates<<"updates"<<endl;
+			//take action according to updates received
 	}
 }
 
 
 int main(int argc, char *argv[]){
-	Argument args;//= new Argument();
-	//5,2,0.2,0.7,0.9,1	
-	args.N=5;//argv[1];
-	args.K=2;//argv[2];
-	args.p=0.2;//argv[3];
-	args.q=0.7;//argv[4];
-	args.r=0.9;//argv[5];
-	args.t=1;//argv[6];
+	Argument args;
+	//5,2,0.2,0.7,0.9,1
+	sscanf(argv[1],"%d",&args.N);
+	sscanf(argv[2],"%d",&args.K);
+	sscanf(argv[3],"%f",&args.p);
+	sscanf(argv[4],"%f",&args.q);
+	sscanf(argv[5],"%f",&args.r);
+	sscanf(argv[6],"%f",&args.t);
+
 	cout<<"0\n"<<std::flush;
+
 	simpleAgent(args);
 	return 0;
 }
