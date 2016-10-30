8f863c39d3d5574433d3cdf3724649c2287a5cc4
rajatsurana <rajat.surana123@gmail.com>
2016-10-30 15:32:17 +0530

    read update parser

--- Agent.cpp
+++ Agent.cpp
@@ -16,13 +16,13 @@ Argument::Argument(){
 }
 class State{
     public:
-    	int N;                        //number of floors
-    	int K;                        //number of elevators
-    	vector<int> pos;							// = [0]*K  initial positions of all elevators
-    	vector<int> BU;								// = [0]*N  # button up on each floor   (always 0 for top floor)
-    	vector<int> BD;              	// button down on each floor (always 0 for first floor)
-    	vector<vector<int> >BF;				//[K][N]  = [[0]*N for i in range(K)] -- floor buttons pressed inside elevator, for each elevator
-    	vector<int> dirn;							//1 or 0 for up or down---dirn for all elevators
+    	int N;							//number of floors
+    	int K;							//number of elevators
+    	vector<int> pos;				// = [0]*K  initial positions of all elevators
+    	vector<int> BU;					// = [0]*N  # button up on each floor   (always 0 for top floor)
+    	vector<int> BD;					// button down on each floor (always 0 for first floor)
+    	vector<vector<int> >BF;			//[K][N]  = [[0]*N for i in range(K)] -- floor buttons pressed inside elevator, for each elevator
+    	vector<int> dirn;				//1 or 0 for up or down---dirn for all elevators
     	State(int N, int K):pos(K),BU(N), BD(N),BF(K),dirn(K){
     	    for(int l=0;l<K;l++)
                 for(int i=0;i<N;i++){
@@ -33,17 +33,15 @@ class State{
 void simpleAgent(Argument args){
 	State state(args.N,args.K);
 	string ready;
-  getline(cin,ready);
-
-  cerr<<ready<<"ready"<<endl;
-
+  	getline(cin,ready);
+  	cerr<<ready<<"ready"<<endl;
 	vector<string> repeat;
 	for(int i=0;i<args.N-1; i++){repeat.push_back("AU");repeat.push_back("AOU");}
-		repeat.push_back("AOD");
+	repeat.push_back("AOD");
 	for(int i=0;i<args.N-1; i++){repeat.push_back("AD");repeat.push_back("AOD");}
-		repeat.push_back("AOU");
-    int i = 0;
-    while(1){
+	repeat.push_back("AOU");
+	int i = 0;
+	while(1){
 		vector<string> actions;
 		string suff="";
 		stringstream sss;
@@ -55,46 +53,162 @@ void simpleAgent(Argument args){
 		}
         //actions = ['AOU' + str(k+1) for k in range(args.K)]
 		string actionsOut="";
-        for (int l=0;l< args.K;l++){
-            if (i>(args.N/args.K+1)*l*2){
+		for (int l=0;l< args.K;l++){
+	        	if (i>(args.N/args.K+1)*l*2){
 				string end="";
 				int ll=l+1;
 				stringstream ss;
 				ss<< ll;ss>>end;
-        actions[l] = repeat[(i - (args.N/args.K+1)*l*2 - 1) % repeat.size()] +end;//+ string(1,1);
-                //cerr<<(char)(l+1)<<" l+1 "<<l+1<<endl;
+			        actions[l] = repeat[(i - (args.N/args.K+1)*l*2 - 1) % repeat.size()] +end;//+ string(1,1);
+	                //cerr<<(char)(l+1)<<" l+1 "<<l+1<<endl;
 			}
 		}
 		actionsOut= actions[0];
 		for(int ll=1;ll<actions.size();ll++){
 			actionsOut+= " "+ actions[ll];
 		}
-        i+=1;
+	    i+=1;
 		cout<<actionsOut<<'\n'<<flush;
 		string updates;
-  	getline(cin,updates);
+	  	getline(cin,updates);
 		cerr<<updates<<"update"<<endl;
 		istringstream iss(updates);
 		vector<string> result;
-		for(string s; iss >> s; )	result.push_back(s);
+		for(string s; iss >> s;)result.push_back(s);
 		cerr<<result[0]<<"res"<<endl;
-		//take action according to updates received
+		int length = result.size();
+		if (length ==1){
+			if(result[0]=="0"){
+
+			}else{
+				if(result[0][0]=='B'){
+					if(result[0][1]=='D'){
+						int floor = (int)result[0][3]- (int)'0';
+						state.BD[floor]=1;
+					}else if(result[0][1]=='U'){
+						int floor = (int)result[0][3]- (int)'0';
+						state.BU[floor]=1;
+					}else if(result[0][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[0][2]- (int)'0';
+							int lift = (int)result[0][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+			}
+		}else if(length==2){
+			if(result[0]=="0"){
+				if(result[1][0]=='B'){
+					if(result[1][1]=='D'){
+						int floor = (int)result[1][3]- (int)'0';
+						state.BD[floor]=1;
+					}else if(result[1][1]=='U'){
+						int floor = (int)result[1][3]- (int)'0';
+						state.BU[floor]=1;
+					}else if(result[1][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[1][2]- (int)'0';
+							int lift = (int)result[1][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+			}else{
+				if(result[0][0]=='B'){
+					if(result[0][1]=='D'){
+						int floor = (int)result[0][3]- (int)'0';
+						state.BD[floor]=1;
+					}else if(result[0][1]=='U'){
+						int floor = (int)result[0][3]- (int)'0';
+						state.BU[floor]=1;
+					}else if(result[0][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[0][2]- (int)'0';
+							int lift = (int)result[0][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+			}
+		}else if(length==3){//length==3
+			if(result[0]=="0"){
+				if(result[1][0]=='B'){
+					if(result[1][1]=='D'){
+						int floor = (int)result[1][3]- (int)'0';
+						state.BD[floor]=1;
+					}else if(result[1][1]=='U'){
+							int floor = (int)result[1][3]- (int)'0';
+							state.BU[floor]=1;
+					}else if(result[1][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[1][2]- (int)'0';
+							int lift = (int)result[1][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+				if(result[2][0]=='B'){
+					if(result[2][1]=='D'){
+						int floor = (int)result[2][3]- (int)'0';
+						state.BU[floor]=1;
+					}else if(result[2][1]=='U'){
+						int floor = (int)result[2][3]- (int)'0';
+						state.BU[floor]=1;
+					}else if(result[2][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[2][2]- (int)'0';
+							int lift = (int)result[2][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+			}else{
+				if(result[0][0]=='B'){
+					if(result[0][1]=='D'){
+						int floor = (int)result[0][3]- (int)'0';
+						state.BD[floor]=1;
+					}else if(result[0][1]=='U'){
+						int floor = (int)result[0][3]- (int)'0';
+						state.BU[floor]=1;
+					}else if(result[0][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[0][2]- (int)'0';
+							int lift = (int)result[0][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+				if(result[1][0]=='B'){
+					if(result[1][1]=='D'){
+						int floor = (int)result[1][3]- (int)'0';
+						state.BD[floor]=1;
+					}else if(result[1][1]=='U'){
+							int floor = (int)result[1][3]- (int)'0';
+							state.BU[floor]=1;
+					}else if(result[1][1]=='_'){
+						if(args.K<10 && args.N<10){
+							int button= (int)result[1][2]- (int)'0';
+							int lift = (int)result[1][4]- (int)'0';
+							state.BF[button][lift]=1;
+						}
+					}
+				}
+			}
+		}
 	}
+		//update state
+		//take action according to policy for states
 }
-
-
 int main(int argc, char *argv[]){
-	Argument args;
-	//5,2,0.2,0.7,0.9,1
+	Argument args;//5,2,0.2,0.7,0.9,1
 	sscanf(argv[1],"%d",&args.N);
 	sscanf(argv[2],"%d",&args.K);
 	sscanf(argv[3],"%f",&args.p);
 	sscanf(argv[4],"%f",&args.q);
 	sscanf(argv[5],"%f",&args.r);
 	sscanf(argv[6],"%f",&args.t);
-
 	cout<<"0\n"<<flush;
-
 	simpleAgent(args);
 	return 0;
 }
